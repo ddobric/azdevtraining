d57e82877347b6e78c8133f81d4ecdfafb265f1a
Damir Dobric <ddobric@daenet.com>
2021-07-03 10:12:37 +0200

    commit appservice sample

---
+++ AppService/WebJobSample/Function.cs
@@ -0,0 +1,29 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace WebJobsSDKSample
+{
+    public class Functions
+    {
+        /// <summary>
+        /// Multiple triggers.
+        /// To test this:
+        /// 1. Upload some file to 'democontainer'. I.e.: desktop.ini
+        /// 2. Craete a new message in the queue 'inqueue' with content "desktop.ini"
+        /// This will trigger function when message arrives. Blob binding will be activated to read the 
+        /// blob with the name contained in the message content.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="myBlob"></param>
+        /// <param name="logger"></param>
+        public static void ProcessQueueMessage(
+            [QueueTrigger("jobqueue")] string message,
+            [Blob("democontainer/{queueTrigger}", FileAccess.Read, Connection = "StorageConnStr")] Stream myBlob, ILogger logger)
+        {
+            logger.LogInformation($"Blob name:{message} \n Size: {myBlob?.Length} bytes");
+        }
+
+      
+    }
+}
---
+++ AppService/WebJobs-old/WebJobs/Functions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace WebJobs
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+
+
+        public static void RunQueueReader([QueueTrigger("myqueue-items", Connection = "QueueConnStr")]string myQueueItem,
+            TraceWriter log)
+        {
+            log.Info($"C# Queue trigger function processed: {myQueueItem}");
+        }
+
+
+        public static void RunSbQueueReader(
+           [ServiceBusTrigger("queueIn", AccessRights.Listen, Connection = "SbConnStr")]BrokeredMessage inMsg,
+           [ServiceBus("queueOut", AccessRights.Send, Connection = "SbConnStr")]out BrokeredMessage outMsg,
+           TraceWriter log)
+        {
+            log.Info($"C# ServiceBus queue trigger v1 function processed message: {inMsg.MessageId}");
+
+            Stream stream = inMsg.GetBody<Stream>();
+            StreamReader reader = new StreamReader(stream);
+            string newBody = reader.ReadToEnd();
+
+            outMsg = new BrokeredMessage(newBody);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timer"></param>
+        /// <param name="log"></param>
+        /// <param name="logger"></param>
+        public static void RunTimer([TimerTrigger("%CRONEEXPRESSION%")] TimerInfo timer, TextWriter log/*, ILogger logger*/)
+        {
+            log.Write("Timer Executed");
+        }
+    }
+}
