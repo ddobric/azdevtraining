aa34edc3bb855493883d395a112dcfa2662e005b
Damir Dobric <ddobric@daenet.com>
2021-07-03 16:09:36 +0200

    azfure functions demos.

--- AzFunctions/AzFuncSample-DotNETCore/Function1.cs
+++ AzFunctions/AzFuncSample-DotNETCore/Function1.cs
@@ -9,11 +9,11 @@ namespace AzFuncSamples
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([QueueTrigger("myqueue-items")]string myQueueItem,
-            [Queue("azfnc-outqueue") ]CloudQueue cloudQueue, ILogger log)
+        public static void Run([QueueTrigger("inputq")]string myQueueItem,
+            [Queue("outputq") ]CloudQueue cloudQueue, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-            cloudQueue.AddMessageAsync(new CloudQueueMessage(DateTime.Now.ToString()));
+            cloudQueue.AddMessageAsync(new CloudQueueMessage($"Message '{myQueueItem}' received at: {DateTime.Now.ToString()}"));
         }
     }
 }
