e0b855f84ff49c205df83c81b9f236987249d0ab
Damir Dobric <ddobric@daenet.com>
2021-07-03 19:31:42 +0200

    Added Storage Samples

---
+++ Storage/QueueStorageSamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace QueueStorageSamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Azure Queues World!");
+
+            QueueStorageSamples samples = new QueueStorageSamples("DefaultEndpointsProtocol=https;AccountName=damiraztraining;AccountKey=EyazJ87KOZG/E8oWTtQxm6DkF4yc0WNF7FvlF4TFHwt9Cl0Z0GQ8UJjiQXtL5zEj/DTLu4KlnUlb1asomxpg3A==;EndpointSuffix=core.windows.net");
+
+            //await samples.CreateQueue();
+
+            //await samples.SendMessages();
+
+            //await samples.ReceiveMessages();
+
+            //await samples.SendReceiveMessagesAdvanced();
+
+            //await samples.DeleteQueue();
+
+            await samples.SendManyMessages();
+
+            //await samples.ReceiveManyMessages();
+
+        }
+    }
+}
