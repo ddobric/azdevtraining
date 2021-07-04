28f677409243159cb1fc8eb640730c578c5d8b84
Damir Dobric <ddobric@daenet.com>
2021-07-04 12:13:06 +0200

    Added samples for EventHub

---
+++ DistributedSystems/EventHub/DotNet5/EventHubReceiver/Program.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventHubReceiver
+{
+    /// <summary>
+    /// https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/eventhub/Azure.Messaging.EventHubs/samples/Sample05_ReadingEvents.md
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello EventHub Receiver!");
+
+            var connectionString = "Endpoint=sb://damirtraining.servicebus.windows.net/;SharedAccessKeyName=read;SharedAccessKey=BJW8jfF9F7J8SZX0nLvOYShnqCTrb8hjnxj6MnWCxco=;EntityPath=myhub";
+            var eventHubName = "myhub";
+            var consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            var consumer = new EventHubConsumerClient(
+                consumerGroup,
+                connectionString,
+                eventHubName);
+
+            try
+            {
+                using CancellationTokenSource cancellationSource = new CancellationTokenSource();
+                cancellationSource.CancelAfter(TimeSpan.FromSeconds(300));
+
+                DateTimeOffset oneHourAgo = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromHours(1));
+                EventPosition startingPosition = EventPosition.FromEnqueuedTime(oneHourAgo);
+
+                string firstPartition = (await consumer.GetPartitionIdsAsync(cancellationSource.Token)).First();
+
+                while (true)
+                {
+                    //await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync(startReadingAtEarliestEvent: true, cancellationToken: cancellationSource.Token))
+
+                    await foreach (PartitionEvent partitionEvent in consumer.ReadEventsFromPartitionAsync(
+                    firstPartition,
+                    startingPosition,
+                    cancellationSource.Token))
+                    {
+                        string readFromPartition = partitionEvent.Partition.PartitionId;
+                        byte[] eventBodyBytes = partitionEvent.Data.EventBody.ToArray();
+
+                        Console.WriteLine($"Read event of length { eventBodyBytes.Length } from { readFromPartition }");
+                    }
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // This is expected if the cancellation token is
+                // signaled.
+            }
+            finally
+            {
+                await consumer.CloseAsync();
+            }
+
+        }
+    }
+}
---
+++ DistributedSystems/EventHub/DotNet5/EventPublisher/Program.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPublisher
+{
+    /// <summary>
+    /// https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/eventhub/Azure.Messaging.EventHubs/samples/Sample04_PublishingEvents.md
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello EventHub publisher");
+
+            var connectionString = "Endpoint=sb://damirtraining.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=4XlB1mrqsrz4Ik52oqPeHzXd9ERPYUDwh125unCsI8Y=;EntityPath=myhub";
+            var eventHubName = "myhub";
+
+            var producer = new EventHubProducerClient(connectionString, eventHubName);
+
+            try
+            {
+                string firstPartition = (await producer.GetPartitionIdsAsync()).First();
+
+                var batchOptions = new CreateBatchOptions
+                {
+                    //PartitionId = firstPartition,       
+                    PartitionKey = "DEVICE"
+                };
+
+                using (var eventBatch = await producer.CreateBatchAsync(batchOptions))
+                {
+                    for (var index = 0; index < 500; ++index)
+                    {
+                        var serData = JsonConvert.SerializeObject(new
+                        {
+                            dateTime = DateTime.Now,
+                            minForce = 123.45,
+                            maxForce = 345.21,
+                            currentForce = 111.32
+                        });
+
+                        var eventBody = new BinaryData(UTF8Encoding.UTF8.GetBytes(serData));
+
+                        var eventData = new EventData(eventBody);
+
+                        //
+                        // Add some custome metadata.
+                        eventData.Properties.Add("EventType", "daenet.com/azuretraining");
+                        eventData.Properties.Add("priority", 1);
+                        eventData.Properties.Add("counter", index);
+
+                        if (!eventBatch.TryAdd(eventData))
+                        {
+                            throw new Exception($"The event at { index } could not be added.");
+                        }
+                    }
+
+                    await producer.SendAsync(eventBatch);
+                }
+            }
+            finally
+            {
+                await producer.CloseAsync();
+            }
+
+        }
+    }
+}
