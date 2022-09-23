39846640b03d924be5695c5c300e27dce05dc954
Damir Dobric <ddobric@daenet.de>
2022-09-23 17:27:03 +0200

    upgrade to net60

--- AzFunctions/FunctionAppSample/FunctionApp1/Function2.cs
+++ AzFunctions/FunctionAppSample/FunctionApp1/Function2.cs
@@ -8,7 +8,7 @@ namespace FunctionApp1
     public class Function2
     {
         [FunctionName("Function2")]
-        public void Run([QueueTrigger("myqueue-items", Connection = "ConnStr")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
